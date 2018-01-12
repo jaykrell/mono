cd4bf4316fc901550d5f412fb9c13a14cd730d46
Jay Krell <jaykrell@microsoft.com>
2018-01-11 20:16:51 -0800

    Fix some but not all of the problems in Authenticode signing. Problems fixed:  - non-support of PE32+  - assumption that headers fit in 4k  - lack of validation of 10b/20b magic number  - lack of validation of NumberOfRvasAndSizes
    Problems not fixed:
     - Failure to ignore data after the last section.
     - Broken and unnecessary code related to coff symbol table.
     - Small 4k buffering.
    
    Still to do: testing

--- mcs/class/Mono.Security/Mono.Security.Authenticode/AuthenticodeBase.cs
+++ mcs/class/Mono.Security/Mono.Security.Authenticode/AuthenticodeBase.cs
@@ -31,6 +31,29 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 
+/* FIXME
+
+This code has a number of structural and correctness problems.
+
+1. The original code was structured to read 4K blocks and assume
+the headers are within the first 4K block. This is an invalid assumption,
+but usually works, i.e. as long as there isn't a creative MS-DOS header.
+The right design is to allow a few exclusions anywhere in the file, at
+arbitrary offset and spanning arbitrary offsets.
+To make a smaller change, the code now grows the block size to accomodate
+the headers, and then resets it back to the original 4K.
+
+2. It is likely that 4K is too small to be performant on modern systems.
+
+3. The handling of coffSymbolTableOffset is seems bogus and incorrect.
+It should be removed.
+
+4. The code ignores this part of the specification:
+	"The area past the last section (defined by highest offset) is not hashed."
+It is concievable the author thought coffSymbolTableOffset is related
+to this, but it is not.
+*/
+
 namespace Mono.Security.Authenticode {
 
 	// References:
@@ -65,9 +88,28 @@ namespace Mono.Security.Authenticode {
 		private int dirSecuritySize;
 		private int coffSymbolTableOffset;
 
-		public AuthenticodeBase ()
+		void ResetBlockSize ()
 		{
 			fileblock = new byte [4096];
+		}
+
+		bool IncreaseBlockSize ()
+		{
+			// FIXME
+			// Given the design, don't consume too much
+			// memory for rare files. There is a design
+			// that accomodates them better, but 4K will usually work here.
+			if (BlockSize > (64 * 1024 * 1024))
+				return false;
+			fileblock = new byte [BlockSize * 2];
+			return true;
+		}
+
+		internal int BlockSize => fileblock.Length;
+
+		public AuthenticodeBase ()
+		{
+			ResetBlockSize ();
 		}
 
 		internal int PEOffset {
@@ -121,9 +163,66 @@ namespace Mono.Security.Authenticode {
 		}
 
 		internal int ProcessFirstBlock ()
+		// Wrapper around ProcessFirstBlockHelper to void
+		// indenting it.
 		{
+			while (true)
+			{
+				int error = ProcessFirstBlockHelper();
+				if (error != -1)
+					return error;
+				if (!IncreaseBlockSize())
+					return -1;
+			}
+		}
+
+		// constants in ProcessFirstBlockHelper
+		const int MsdosHeaderSize = 64;
+		const int PESignatureSize = 4;
+		const int FileHeaderSize = 20;
+		const int OptionalHeader = PESignatureSize + FileHeaderSize; // not optional
+		const int IMAGE_NT_OPTIONAL_HDR32_MAGIC = 0x10b;
+		const int IMAGE_NT_OPTIONAL_HDR64_MAGIC = 0x20b;
+		const uint IMAGE_DIRECTORY_ENTRY_SECURITY = 4;
+
+		// FIXME locals in ProcessFirstBlockHelper, captured by reference in MinSize
+		int offset_NumberOfRvaAndSizes = 92; // or 108
+		int offset_SecurityDirectory = 128; // or 144
+
+		int MinSize() // FIXME lambda in ProcessFirstBlockHelper with reference capture
+		{
+			return peOffset + OptionalHeader + offset_SecurityDirectory + 8;
+		}
+
+		internal int ProcessFirstBlockHelper ()
+		// 0: success; some historical values are preserved
+		// >0: error (historical)
+		// <0: internal: grow buffer and retry (mitigate historical design problem)
+		{
+/*
+PE file is structured partly as follows:
+64 bytes {
+  2 byte MZ
+  ...
+  4 byte offset to NT headers
+}
+NT headers {
+  DWORD                 Signature;  	 PE\0\0
+  IMAGE_FILE_HEADER     FileHeader;      20 bytes on all architectures
+  IMAGE_OPTIONAL_HEADER OptionalHeader;	 starts with 2 byte magic, and eventually varies 32/64
+}
+The "optional" header is not actually optional.
+*/
+			// locals in ProcessFirstBlockHelper
+			peOffset = MsdosHeaderSize; // minimum, will typically increase
+			offset_NumberOfRvaAndSizes = 92; // or 108
+			offset_SecurityDirectory = 128; // or 144
+
+			//System.Console.WriteLine("ProcessFirstBlock 1");
 			if (fs == null)
 				return 1;
+
+			//System.Console.WriteLine("ProcessFirstBlock 2");
 
 			fs.Position = 0;
 			// read first block - it will include (100% sure) 
@@ -133,6 +232,8 @@ namespace Mono.Security.Authenticode {
 			if (blockLength < 64)
 				return 2;	// invalid PE file
 
+			//System.Console.WriteLine("ProcessFirstBlock 3");
+
 			// 1. Validate the MZ header informations
 			// 1.1. Check for magic MZ at start of header
 			if (BitConverterLE.ToUInt16 (fileblock, 0) != 0x5A4D)
@@ -140,15 +241,62 @@ namespace Mono.Security.Authenticode {
 
 			// 1.2. Find the offset of the PE header
 			peOffset = BitConverterLE.ToInt32 (fileblock, 60);
-			if (peOffset > fileblock.Length) {
-				// just in case (0.1%) this can actually happen
-				string msg = String.Format (Locale.GetText (
-					"Header size too big (> {0} bytes)."),
-					fileblock.Length);
-				throw new NotSupportedException (msg);
-			}
 			if (peOffset > fs.Length)
 				return 4;
+
+			// File big enough?
+			if (fs.Length < MinSize()) {
+				System.Console.WriteLine($"ProcessFirstBlock3 smallFile:0x{fs.Length:X} MinSize:0x{MinSize():X}");
+				return -3;
+			}
+
+			// Buffer big enough? If not, grow it and retry.
+			if (BlockSize < MinSize()) {
+				System.Console.WriteLine($"ProcessFirstBlock4 smallBuffer BlockSize:0x{BlockSize:X} MinSize:0x{MinSize():X}");
+				return -1;
+			}
+
+			// Successful read big enough? (How to get coverage?)
+			if (blockLength < MinSize()) {
+				System.Console.WriteLine($"ProcessFirstBlock5 smallRead:0x{blockLength:X} MinSize:0x{MinSize():X}");
+				return -5;
+			}
+
+			uint magic = BitConverterLE.ToUInt16 (fileblock, peOffset + OptionalHeader);
+			if (magic ==  IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
+				// nothing, ok
+			}
+			else if (magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
+				offset_NumberOfRvaAndSizes = 108;
+				offset_SecurityDirectory = 144;
+			} else {
+				System.Console.WriteLine ($"ProcessFirstBlock6 invalid PE/10b/20b magic number magic:0x{magic:X}");
+				return -6;
+			}
+
+			// File big enough?
+			if (fs.Length < MinSize()) {
+				System.Console.WriteLine($"ProcessFirstBlock8 smallFile:0x{fs.Length:X} MinSize:0x{MinSize():X}");
+				return -8;
+			}
+
+			// Buffer big enough? If not, grow it and retry.
+			if (BlockSize < MinSize()) {
+				System.Console.WriteLine($"ProcessFirstBlock9 smallBuffer BlockSize:0x{BlockSize:X} MinSize:0x{MinSize():X}");
+				return -1;
+			}
+
+			// Successful read big enough? (How to get coverage?)
+			if (blockLength < MinSize()) {
+				System.Console.WriteLine($"ProcessFirstBlock10 smallRead:0x{blockLength:X} MinSize:0x{MinSize():X}");
+				return -10;
+			}
+
+			uint numberOfRvaAndSizes = BitConverterLE.ToUInt32 (fileblock, peOffset + OptionalHeader + offset_NumberOfRvaAndSizes);
+			if (numberOfRvaAndSizes < IMAGE_DIRECTORY_ENTRY_SECURITY) {
+				System.Console.WriteLine($"ProcessFirstBlock11 numberOfRvaAndSizes < IMAGE_DIRECTORY_ENTRY_SECURITY 0x{numberOfRvaAndSizes:X}");
+				return -11;
+			}
 
 			// 2. Read between DOS header and first part of PE header
 			// 2.1. Check for magic PE at start of header
@@ -157,12 +305,20 @@ namespace Mono.Security.Authenticode {
 				return 5;
 
 			// 2.2. Locate IMAGE_DIRECTORY_ENTRY_SECURITY (offset and size)
-			dirSecurityOffset = BitConverterLE.ToInt32 (fileblock, peOffset + 152);
-			dirSecuritySize = BitConverterLE.ToInt32 (fileblock, peOffset + 156);
+			dirSecurityOffset = BitConverterLE.ToInt32 (fileblock, peOffset + OptionalHeader + offset_SecurityDirectory);
+			dirSecuritySize = BitConverterLE.ToInt32 (fileblock, peOffset + OptionalHeader + offset_SecurityDirectory + 4);
+
+			if (dirSecurityOffset + dirSecuritySize > fs.Length) {
+				System.Console.WriteLine($"ProcessFirstBlock13 securityDirectotyOutOfRange fs.Length:0x{fs.Length:X} dirSecurityOffset:{dirSecurityOffset} dirSecuritySize:{dirSecuritySize}");
+				return -13;
+			}
 
 			// COFF symbol tables are deprecated - we'll strip them if we see them!
 			// (otherwise the signature won't work on MS and we don't want to support COFF for that)
 			coffSymbolTableOffset = BitConverterLE.ToInt32 (fileblock, peOffset + 12);
+
+			// FIXME validation and testing of coffSymbolTableOffset != 0
+			// I don't see why this code is ever here.
 
 			return 0;
 		}
@@ -194,8 +350,10 @@ namespace Mono.Security.Authenticode {
 			int addsize = 0;
 			// minus any authenticode signature (with 8 bytes header)
 			if (dirSecurityOffset > 0) {
-				// it is also possible that the signature block 
-				// starts within the block in memory (small EXE)
+				// FIXME This assumes if the signature
+				// starts in the first block, that it does
+				// not span blocks. Typically the signature
+				// is at the end.
 				if (dirSecurityOffset < blockLength) {
 					blockLength = dirSecurityOffset;
 					n = 0;
@@ -203,6 +361,7 @@ namespace Mono.Security.Authenticode {
 					n = dirSecurityOffset - blockLength;
 				}
 			} else if (coffSymbolTableOffset > 0) {
+				// FIXME This is messed up but mitigated.
 				fileblock[PEOffset + 12] = 0;
 				fileblock[PEOffset + 13] = 0;
 				fileblock[PEOffset + 14] = 0;
@@ -211,8 +370,8 @@ namespace Mono.Security.Authenticode {
 				fileblock[PEOffset + 17] = 0;
 				fileblock[PEOffset + 18] = 0;
 				fileblock[PEOffset + 19] = 0;
-				// it is also possible that the signature block 
-				// starts within the block in memory (small EXE)
+
+				// FIXME This is also incorrect but rarely/never occurs.
 				if (coffSymbolTableOffset < blockLength) {
 					blockLength = coffSymbolTableOffset;
 					n = 0;
@@ -227,16 +386,15 @@ namespace Mono.Security.Authenticode {
 				n = fs.Length - blockLength;
 			}
 
-			// Authenticode(r) gymnastics
-			// Hash from (generally) 0 to 215 (216 bytes)
-			int pe = peOffset + 88;
+			// Skip the checksum and the security directory.
+			int pe = peOffset + OptionalHeader + 64;
 			hash.TransformBlock (fileblock, 0, pe, fileblock, 0);
 			// then skip 4 for checksum
 			pe += 4;
-			// Continue hashing from (generally) 220 to 279 (60 bytes)
-			hash.TransformBlock (fileblock, pe, 60, fileblock, pe);
+			hash.TransformBlock (fileblock, pe, peOffset + OptionalHeader + offset_SecurityDirectory - pe, fileblock, pe);
 			// then skip 8 bytes for IMAGE_DIRECTORY_ENTRY_SECURITY
-			pe += 68;
+			pe = peOffset + OptionalHeader + offset_SecurityDirectory;
+			pe += 8;
 
 			// everything is present so start the hashing
 			if (n == 0) {
@@ -246,6 +404,8 @@ namespace Mono.Security.Authenticode {
 			else {
 				// hash the last part of the first (already in memory) block
 				hash.TransformBlock (fileblock, pe, blockLength - pe, fileblock, pe);
+
+				ResetBlockSize ();
 
 				// hash by blocks of 4096 bytes
 				long blocks = (n >> 12);
