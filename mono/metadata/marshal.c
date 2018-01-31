7a564e49a743b9f085a25a8412ee94f8561b12ee
Jay Krell <jaykrell@microsoft.com>
2018-01-31 04:27:30 -0800

    [Cplusplus] Cast between RawHandle and ObjectHandle.

--- mono/metadata/marshal.c
+++ mono/metadata/marshal.c
@@ -12429,9 +12429,9 @@ static MonoObjectHandle
 mono_icall_handle_new (gpointer rawobj)
 {
 #ifdef MONO_HANDLE_TRACK_OWNER
-	return mono_handle_new (rawobj, "<marshal args>");
+	return (MonoObjectHandle)mono_handle_new ((MonoObject*)rawobj, "<marshal args>");
 #else
-	return mono_handle_new (rawobj);
+	return (MonoObjectHandle)mono_handle_new ((MonoObject*)rawobj);
 #endif
 }
 
@@ -12439,8 +12439,8 @@ static MonoObjectHandle
 mono_icall_handle_new_interior (gpointer rawobj)
 {
 #ifdef MONO_HANDLE_TRACK_OWNER
-	return mono_handle_new_interior (rawobj, "<marshal args>");
+	return (MonoObjectHandle)mono_handle_new_interior ((MonoObject*)rawobj, "<marshal args>");
 #else
-	return mono_handle_new_interior (rawobj);
+	return (MonoObjectHandle)mono_handle_new_interior ((MonoObject*)rawobj);
 #endif
 }
