676f844bb4edf889f0e2e94284bc9830fdcfa1a3
Jay Krell <jaykrell@microsoft.com>
2018-02-27 02:37:53 -0800

    [Coop] Convert ves_icall_System_Threading_Thread_ResetAbort to coop/handle.

--- mono/metadata/exception-internals.h
+++ mono/metadata/exception-internals.h
@@ -64,4 +64,7 @@ mono_exception_new_thread_interrupted (MonoError* error);
 MonoExceptionHandle
 mono_exception_new_thread_abort (MonoError* error);
 
+MonoExceptionHandle
+mono_exception_new_thread_state (const char *msg, MonoError *error);
+
 #endif
