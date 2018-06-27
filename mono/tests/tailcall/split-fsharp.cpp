a69625210992599ef472a0c7806ba873e96af84e
Jay Krell <jaykrell@microsoft.com>
2018-06-27 03:24:30 -0700

    cleanup

--- mono/tests/tailcall/split-fsharp.cpp
+++ mono/tests/tailcall/split-fsharp.cpp
@@ -16,78 +16,78 @@ g++ split-fsharp.cpp -std=c++11 && ./a.out < fsharp-deeptail.il
 #include <string.h>
 using namespace std;
 
-#define count(a) (sizeof(a)/sizeof((a)[0]))
-
-int main()
+int main ()
 {
-struct test_t
-{
-	string name;
-	vector<string> content;
-};
-string line;
-vector<string> prefix;
-vector<test_t> tests;
-test_t test_dummy;
-test_t *test = &test_dummy;
-vector<string> suffix;
-
-while (getline (cin, line))
-{
-	prefix.push_back(line);
-	if (line == ".entrypoint")
-		break;
-}
-
-while (getline (cin, line))
-{
-	// tests are delimited by empty lines
-	if (line == "")
+	struct test_t
 	{
-		tests.resize(tests.size() + 1);
-		test = &tests.back();
-		assert(getline(cin, line));
-		// tests start with ldstr
-		//printf("%s\n", line.c_str());
-#define CONSTANT_STRING_AND_LENGTH(x) x, count(x) - 1
-		bool ldstr = memcmp(line.c_str(), CONSTANT_STRING_AND_LENGTH("ldstr \"")) == 0;
-		bool ldc_i4_0 = memcmp(line.c_str(), CONSTANT_STRING_AND_LENGTH("ldc.i4.0")) == 0;
-		assert(ldstr || ldc_i4_0);
-		if (ldc_i4_0)
-		{
-			suffix.push_back(line);
-			break;
-		}
-		string name1 = &line[7];
-		*strchr((char*)name1.c_str(), '"') = 0; // truncate at quote
-		test->name = name1.c_str();
-		for (auto& c: test->name)
-		{
-			if (strchr("<>", c))
-				c = '_';
-		}
-		//printf("%s\n", test->name.c_str());
-	}
-	test->content.push_back(line);
-}
-while (getline (cin, line))
-{
-	suffix.push_back(line);
-}
-for (auto& t: tests)
-{
-	if (t.name == "")
-		continue;
-	//printf("%s\n", t.name.c_str());
-	ofstream output("fsharp-deeptail-" + t.name + ".il");
-	for (auto &a: prefix)
-		output << a.c_str() << endl;
-	output << endl;
-	for (auto &a: t.content)
-		output << a.c_str() << endl;
-	output << endl;
-	for (auto &a: suffix)
-		output << a.c_str() << endl;
+		string name;
+		vector<string> content;
+	};
+	string line;
+	vector<string> prefix;
+	vector<test_t> tests;
+	test_t test_dummy;
+	test_t *test = &test_dummy;
+	vector<string> suffix;
 
-}
+	while (getline (cin, line))
+	{
+		prefix.push_back (line);
+		if (line == ".entrypoint")
+			break;
+	}
+
+	string marker_ldstr = "ldstr \"";	// start of each test, and contains name
+	string marker_ldc_i4_0 = "ldc.i4.0";	// start of suffix
+
+	while (getline (cin, line))
+	{
+		// tests are delimited by empty lines
+		if (line == "")
+		{
+			tests.resize (tests.size () + 1);
+			test = &tests.back ();
+			assert (getline (cin, line));
+			// tests start with ldstr
+			//printf("%s\n", line.c_str());
+			bool ldstr = line.length () >= marker_ldstr.length () && memcmp(line.c_str (), marker_ldstr.c_str (), marker_ldstr.length ()) == 0;
+			bool ldc_i4_0 = line.length () >= marker_ldc_i4_0.length () && memcmp(line.c_str (), marker_ldc_i4_0.c_str (), marker_ldc_i4_0.length ()) == 0;
+			assert (ldstr || ldc_i4_0);
+			if (ldc_i4_0)
+			{
+				suffix.push_back (line);
+				break;
+			}
+			string name1 = &line [marker_ldstr.length ()];
+			*strchr ((char*)name1.c_str (), '"') = 0; // truncate at quote
+			test->name = name1.c_str ();
+			for (auto& c: test->name)
+			{
+				if (strchr("<>", c))
+					c = '_';
+			}
+			//printf("%s\n", test->name.c_str());
+		}
+		test->content.push_back (line);
+	}
+	while (getline (cin, line))
+	{
+		suffix.push_back (line);
+	}
+	for (auto& t: tests)
+	{
+		if (t.name == "")
+			continue;
+		//printf("%s\n", t.name.c_str());
+		ofstream output("fsharp-deeptail-" + t.name + ".il");
+		for (auto &a: prefix)
+			output << a.c_str () << endl;
+		output << endl;
+		for (auto &a: t.content)
+			output << a.c_str () << endl;
+		output << endl;
+		for (auto &a: suffix)
+			output << a.c_str () << endl;
+
+	}
 }
