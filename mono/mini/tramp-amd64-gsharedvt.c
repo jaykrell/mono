2ab31060f97a6cebdc1b633cb9aaab8eb5e678b5
Jay Krell <jaykrell@microsoft.com>
2018-09-11 19:00:55 -0700

    print

--- mono/mini/tramp-amd64-gsharedvt.c
+++ mono/mini/tramp-amd64-gsharedvt.c
@@ -40,8 +40,8 @@
 #define SLOT_COUNT_SHIFT 24
 #define SLOT_COUNT_MASK 0xFF
 
-//#undef DEBUG_AMD64_GSHAREDVT_PRINT
-//#define DEBUG_AMD64_GSHAREDVT_PRINT printf
+#undef DEBUG_AMD64_GSHAREDVT_PRINT
+#define DEBUG_AMD64_GSHAREDVT_PRINT printf
 
 gpointer
 mono_amd64_start_gsharedvt_call (GSharedVtCallInfo *info, gpointer *caller, gpointer *callee, gpointer mrgctx_reg)
