a759133f6db8a33451ea0a4accbfa492608589c8
Jay Krell <jaykrell@microsoft.com>
2018-10-09 11:43:32 -0700

    work in progress

--- mcs/tools/icall-marshal/icall-marshal.cs
+++ mcs/tools/icall-marshal/icall-marshal.cs
@@ -2,12 +2,19 @@ using Mono.Cecil;
 
 public class A
 {
-	public static void Main (string [] args)
-	{
-		ModuleDefinition module = ModuleDefinition.ReadModule (args[0]);
-		foreach (var type in module.Types) {
-			foreach (var m in type.methods) {
-//				System.Console.WriteLine (type.FullName + "." + m.Name);
+       public static void Main (string [] args)
+       {
+		foreach (var arg in args)
+		{
+			ModuleDefinition module = ModuleDefinition.ReadModule (arg);
+			foreach (var type in module.Types)
+			{
+				foreach (var m in type.Methods)
+				{
+					if (!m.IsInternalCall () || !m.IsRuntime ())
+						continue;
+					System.Console.WriteLine ($"{arg} {type.FullName}.{m.Name} Attributes:{m.Attributes} ImplAttributes:{m.ImplAttributes}");
+				}
 			}
 		}
 	}
