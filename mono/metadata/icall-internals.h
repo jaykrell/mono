106d329ef1130802b7dec67a021217ce8f3dcb62
Jay Krell <jaykrell@microsoft.com>
2019-10-16 16:53:22 -0700

    [cxx] Reduce use of <type_traits> on Android.
    In file included from /Users/builder/jenkins/workspace/test-mono-pull-request-amd64-osx-products-sdks-android/mono/metadata/icall.c:42:
    /Users/builder/jenkins/workspace/test-mono-pull-request-amd64-osx-products-sdks-android/mono/metadata/icall-internals.h:114:1: error: C++ requires a type specifier for all declarations
    mono_add_internal_call_with_flags (const char *name, T method, gboolean cooperative)
    ^
    /Users/builder/jenkins/workspace/test-mono-pull-request-amd64-osx-products-sdks-android/mono/metadata/icall-internals.h:116:9: error: cannot initialize return object of type 'int' with an rvalue of type 'void'
            return mono_add_internal_call_with_flags (name, (const void*)method, cooperative);
                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/builder/jenkins/workspace/test-mono-pull-request-amd64-osx-products-sdks-android/mono/metadata/icall-internals.h:127:1: error: C++ requires a type specifier for all declarations
    mono_add_internal_call_internal (const char *name, T method)
    ^
    /Users/builder/jenkins/workspace/test-mono-pull-request-amd64-osx-products-sdks-android/mono/metadata/icall-internals.h:129:9: error: cannot initialize return object of type 'int' with an rvalue of type 'void'
            return mono_add_internal_call_internal (name, (const void*)method);
                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    4 errors generated.

--- mono/metadata/icall-internals.h
+++ mono/metadata/icall-internals.h
@@ -89,16 +89,24 @@ mono_lookup_internal_call_full_with_flags (MonoMethod *method, gboolean warn_on_
 #include <type_traits>
 
 template <typename T>
+#if HOST_ANDROID // type_traits does not work well
+inline T
+#else
 inline typename std::enable_if<std::is_function<T>::value ||
 			       std::is_function<typename std::remove_pointer<T>::type>::value >::type
+#endif
 mono_add_internal_call_with_flags (const char *name, T method, gboolean cooperative)
 {
 	return mono_add_internal_call_with_flags (name, (const void*)method, cooperative);
 }
 
 template <typename T>
+#if HOST_ANDROID // type_traits does not work well
+inline T
+#else
 inline typename std::enable_if<std::is_function<T>::value ||
 			       std::is_function<typename std::remove_pointer<T>::type>::value >::type
+#endif
 mono_add_internal_call_internal (const char *name, T method)
 {
 	return mono_add_internal_call_internal (name, (const void*)method);
